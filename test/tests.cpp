6432d70015448bb392067bd880a0a09e9b52d11e
pozvas <pozvas03@mail.ru>
2024-04-06 13:36:50 +0300

    try 4

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,6 +3,9 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <cstdint>
+#include <thread> // NOLINT [build/c++11]
+#include <chrono> // NOLINT [build/c++11]
+
 #include "TimedDoor.h"
 
 
